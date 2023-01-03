ed89938c2c72738f0c7982c8686940f9c5d8f5ba
Ozakpo <meshachozakpo@yahoo.com>
2023-01-03 05:54:05 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints a given phrase
- * Return: 0 if exited properly, or return a not zero value
+ * Return: 0 if exited properly, non zero otherwise
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
