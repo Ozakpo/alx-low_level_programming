232941b61407873da552f5987e6e49af96fb0606
Ozakpo <meshachozakpo@yahoo.com>
2023-01-04 08:21:34 -0800

    5-sqrt_recursion.c alx task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,16 +6,25 @@
   *
   *Return: sqrt of number at -1 for error.
   */
-int _evaluate(int num, int pSqrt)
+int helperFunction(int num, int pSqrt)
 {
-	if (pSqrt == 0 || pSqrt == 1)
-	
+	if ((pSqrt * pSqrt) == num)
+	{
 		return (pSqrt);
 	else if ((pSqrt * pSqrt) > num)
-		return (_evaluate(num + 1, pSqrt);
+		return (-1);
+	else 
+		return (helperFunction(num, pSqrt + 1));
+	}
 }
-	int _sqrt_recursion(int n) /*returns the natural square root of a number*/
-	{
+	/* _sqrt_recursion - returns the natural square root of a number
+	*@n: number to find
+	*
+	*Return: squareroot of n.
+        *-1 if n does not have a natural sqrt.
+        */
+	int _sqrt_recursion(int n)
+{
 	if (n < 0)
 	  return (-1);
 	else
