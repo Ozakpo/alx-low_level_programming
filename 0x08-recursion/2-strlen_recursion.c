d56d405edd564b9c9d088ebc104cedd75c67777e
Ozakpo <meshachozakpo@yahoo.com>
2023-01-04 07:19:23 -0800

    2-strlen_recursion.c alx task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,7 +7,11 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0') /*base condition*/
+	{
 		return (0);
-else 
-	return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
+	}
+         else 
+	 {
+	 	 return (1 + _strlen_recursion(s + 1)); 
+         }
 }
