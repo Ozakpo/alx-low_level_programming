2120e56fc9f0e56a7d0d00f48e0d6ebf6785acb5
Ozakpo <meshachozakpo@yahoo.com>
2023-01-04 09:04:16 -0800

    6-is_prime_number.c alx task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int helperFunction(int num, int i)
+{
+	if (i < num)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (helperFunction(num, i + 1));
+		}
+	}
+	{
+	return (1);
+}
+}
+/**
+ *
+ *
+ *
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{   return (0); }
+	else 
+	{ return (helperFunction(n, 2)); }
+}
